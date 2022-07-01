2917df7629293f67ce1fc134cd89bacd4200e094
unknown <dgray.stlouis@gmail.com>
2022-06-30 19:31:48 -0500

    final commit revB

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 -- Part 1: Test it with SQL
 show columns from job;
 -- Part 2: Test it with SQL
-select name from employer where location="St. Louis City";
+select name from employer where location = "St. Louis City";
 -- Part 3: Test it with SQL
 drop table job;
 -- Part 4: Test it with SQL
